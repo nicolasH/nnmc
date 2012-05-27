485fd1e6d4878b563b36e55921064d62cf29eb5b
Nicolas Hoibian <nicolas.hoibian@gmail.com>
2012-05-27 23:13:06 +0900

    Now using a VirtualHost for the hosting, for less difference between dev and prod

--- local_config.php
+++ local_config.php
@@ -3,7 +3,8 @@
 #define('BASE_PATH', '~niko/tests/w2n-remote/');// leave any trailing slash
 #define('BASE_URI', '~niko/tests/w2n-remote');// Omit any trailing slash
 define('BASE_PATH', '/Users/niko/Sites/nnmc/content_nnmc/');// leave any trailing slash
-define('BASE_URI', '/~niko/nnmc/');// Omit any trailing slash
+define('BASE_URI', '/');// Omit any trailing slash
+#define('BASE_URI', '/~niko/nnmc/');// Omit any trailing slash
 
 $analytics="\t<!-- no analytics on the dev web site-->\n";
 
